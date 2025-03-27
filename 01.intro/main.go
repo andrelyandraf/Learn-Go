d46b7ecabbe722037fb7ccd87f52546da47d9b2b
andre <andreferyana@gmail.com>
2025-03-27 14:57:09 +0700

    chapter 2 variable

---
+++ 01.intro/main.go
@@ -0,0 +1,13 @@
+package main 
+
+import (
+	"fmt"
+	"time"
+)
+
+func main()  {
+	fmt.Println("hello world")
+	fmt.Println("welcome to go world")
+	fmt.Println("enjoy !!")
+	time.Sleep(5 * time.Second)
+}
---
+++ 02. Variable/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	message := "hello world"
+	fmt.Println(message)
+
+	message = "this same var with diferent value"
+	fmt.Println(message)
+}
